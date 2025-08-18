5f697b934f79e4a44faa400e2f2c521615d70a72
Patricio Palacios <ppalacios92@gmail.com>
2025-08-18 18:08:24 -0400

    update setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from pathlib import Path
 
 setup(
-    name='EtabsToPython',
+    name='etabstopython',
     version='0.1.0',
     packages=find_packages(),  
     install_requires=[
